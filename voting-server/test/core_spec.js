731d8092b9ed0c455fc860e0d417728e211638ae
alexthedar <alex.castaneda@gmail.com>
2016-10-21 18:44:02 -0700

    test not passing

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,6 @@
 import {List, Map} from 'immutable'
 import {expect} from 'chai'
-
-import {setEntries} from '../src/core'
+import {setEntries, next} from '../src/core'
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -11,6 +10,29 @@ describe('application logic', () => {
       const nextState = setEntries(state, entries)
       expect(nextState).to.equal(Map({
         entries: List.of('Trainspotting', '28 days later')
+      }))
+    })
+
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 days later']
+      const nextState = setEntries(state, entries)
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 days later')
+      }))
+    })
+  })
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 days later', 'Sunshine')
+      })
+      const nextState = next(state)
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 days later')
+        }),
+        entries: List.of('Sunshine')
       }))
     })
   })
